60ddf3c51b690e7bdda5b625e3e45168096362ce
gvanzyll <gerritvanzyll@gmail.com>
2022-01-09 15:21:25 -0800

    try2

--- pages/index.py
+++ pages/index.py
@@ -16,15 +16,25 @@ column1 = dbc.Col(
         dcc.Markdown(
             """
         
-            ## Thinking about visitng Amsterdam? 
-
-            Get an idea of Airbnb prices in and around the city.
-
+            ## Thinking about visitng Amsterdam?
+             
+            Great choice! Amsterdam is an amazing city with great people, tasty food, and much more.          
+            
+            The one and only downside about visiting Amsterdam is that it can get expensive.
+            
+            I built this app to help people get an idea of accomodation prices before they go.
+            
             Plug in your ideal accomodation criteria and see how much to expect to spend.
+
+            If you want to learn more about how the model works, you can check out my Medium post [here](https://medium.com/@gerrit.van.zyll?p=ae53ab03cbea).
 
             """
         ),
-        dcc.Link(dbc.Button('Predict Prices!', color='primary'),
+        dcc.Link(dbc.Button('Predict Prices!', color='primary',
+                            style={
+                                'fontSize': 25
+                            },
+                            ),
                  href='/predictions')
     ],
     md=4,
